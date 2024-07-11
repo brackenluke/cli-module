a5170045860172f71b466e39ac8073277c842ffe
brackenluke <156158313+brackenluke@users.noreply.github.com>
2024-07-11 11:32:20 -0500

    Update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO employees (employee_name)
+VALUES ('Matthew'),
+       ('Mark'),
+       ('Luke'),
+       ('John');
+
+INSERT INTO departments (employee_id, department)
+VALUES (1, 'Accounting'),
+       (2, 'Sales'),
+       (1, 'Marketing'),
+       (3, 'Management'),
+       (5, 'Housekeeping'),
+       (1, 'Security'),
+       (5, 'Human Resources');
